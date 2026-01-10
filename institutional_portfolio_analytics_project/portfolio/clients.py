25639b7de5fd2c6f0d6713cfa97143c758429756
ArchitMathur14 <065012@fsm.ac.in>
2026-01-11 00:03:10 +0530

    Create clients.py

---
+++ institutional_portfolio_analytics_project/portfolio/clients.py
@@ -0,0 +1,32 @@
+clients = {
+    "DC": {
+        "name": "Defined Contribution Plan",
+        "risk_profile": "High",
+        "target_allocation": {
+            "SPY": 0.70,
+            "AGG": 0.20,
+            "VEA": 0.05,
+            "GLD": 0.05
+        }
+    },
+    "DB": {
+        "name": "Defined Benefit Plan",
+        "risk_profile": "Moderate",
+        "target_allocation": {
+            "SPY": 0.45,
+            "AGG": 0.40,
+            "VEA": 0.10,
+            "GLD": 0.05
+        }
+    },
+    "Endowment": {
+        "name": "Endowment & Foundation",
+        "risk_profile": "Long-term Growth",
+        "target_allocation": {
+            "SPY": 0.60,
+            "AGG": 0.20,
+            "VEA": 0.10,
+            "GLD": 0.10
+        }
+    }
+}
