508c0a0d7a1aafb659eee0cb60725534b1666655
ArchitMathur14 <065012@fsm.ac.in>
2026-01-11 00:09:09 +0530

    Update optimizer.py

--- institutional_portfolio_analytics_project/portfolio/optimizer.py
+++ institutional_portfolio_analytics_project/portfolio/optimizer.py
@@ -4,9 +4,18 @@ def optimize_portfolio(price_data, target_allocation):
     mu = expected_returns.mean_historical_return(price_data)
     S = risk_models.sample_cov(price_data)
 
-    ef = EfficientFrontier(mu, S, weight_bounds=(0.05, 0.70))
-    ef.max_sharpe()
+    ef = EfficientFrontier(mu, S)
 
+    # Add constraints so portfolio stays close to target allocation
+    for asset, target_weight in target_allocation.items():
+        ef.add_constraint(
+            lambda w, asset=asset, tw=target_weight: w[list(mu.index).index(asset)] >= tw - 0.15
+        )
+        ef.add_constraint(
+            lambda w, asset=asset, tw=target_weight: w[list(mu.index).index(asset)] <= tw + 0.15
+        )
+
+    ef.max_sharpe()
     weights = ef.clean_weights()
     performance = ef.portfolio_performance()
 
